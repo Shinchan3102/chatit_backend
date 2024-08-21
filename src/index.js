e722690ca8aabfc8a062d3a6217f982c927f3692
Ankit Kumar <ankitkumardos47@gmail.com>
2024-08-22 03:20:29 +0530

    added websocket

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 'use strict';
 
-module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
+const WebSocket = require('ws');
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
+module.exports = {
+  register({ strapi }) {},
+
+  bootstrap({ strapi }) {
+    const server = strapi.server.httpServer;
+    const wss = new WebSocket.Server({ server });
+
+    wss.on('connection', (ws) => {
+      console.log('New WebSocket connection');
+
+      ws.on('message', (message) => {
+        const textMessage = Buffer.isBuffer(message) ? message.toString() : message;
+        console.log('Received:', textMessage);
+        
+        ws.send(textMessage);
+      });
+
+      ws.on('close', () => {
+        console.log('WebSocket connection closed');
+      });
+
+      ws.on('error', (error) => {
+        console.error('WebSocket error:', error);
+      });
+    });
+  },
 };
